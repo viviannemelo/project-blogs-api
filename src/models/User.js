934a09a8025d9e2fc82df5bf816d43c1178ca263
viviannemelo <viviannemmelo@gmail.com>
2023-05-13 10:37:44 -0300

    Creating User model (task2)

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User', 
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      tableName: 'users',
+      underscored: true,
+      timestamps: false,
+    },
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,{
+      foreignKey: 'userId',
+      as: 'blog_posts'
+    });
+  };
+
+  return User;
+};
