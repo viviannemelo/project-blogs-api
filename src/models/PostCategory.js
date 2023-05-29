dac03b07576c8573a2d062fad80ca59eaf22d741
viviannemelo <viviannemmelo@gmail.com>
2023-05-29 13:47:17 -0300

    Creating PostCategory model (task11)

---
+++ src/models/PostCategory.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define(
+      'PostCategory', 
+      {
+        postId: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        categoryId: {
+          type: DataTypes.STRING,
+        },
+      },
+      {
+        underscored: true,
+        tableName: 'post_categories',
+        timestamps: false,
+      }
+    );
+
+    PostCategory.associate = (model) => {
+        model.BlogPost.belongsToMany(model.Category, {
+            as: 'categories',
+            through: PostCategory,
+            foreignKey: 'categoryId',
+            otherKey: 'postId', 
+        });
+        model.Category.belongsToMany(model.BlogPost, {
+            as: 'blogPost',
+            through: PostCategory,
+            foreignKey: 'postId',
+            otherKey: 'categoryId', 
+        })
+    }
+  
+    return PostCategory;
+};
