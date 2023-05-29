648a1c2b1960f656a94a48a152303e557b111be5
viviannemelo <viviannemmelo@gmail.com>
2023-05-29 13:00:01 -0300

    Creating Category model (task7)

---
+++ src/models/Category.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define(
+      'Category', 
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        name: {
+          type: DataTypes.STRING,
+        },
+      },
+      {
+        tableName: 'categories',
+        timestamps: false,
+      }
+    );
+  
+    return Category;
+};
